3916432660610eb91aa42482a2c12274d24364e1
egasparotto <gasp.eduardo@gmail.com>
2017-05-17 20:14:01 -0300

    Oi

---
+++ app/src/main/java/com/comunidadeapp/minhacomunidade/CadastroUser.java
@@ -0,0 +1,92 @@
+package com.comunidadeapp.minhacomunidade;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class CadastroUser extends AppCompatActivity {
+    private static final String TAG = "EmailPassword";
+    private FirebaseAuth mAuth;
+    private EditText edtSenha;
+    private EditText edtEmail;
+    private Button btnCriar;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_user);
+        Intent Mensagens = new Intent(this,ServicoMensagens.class);
+        startService(Mensagens);
+        mAuth = FirebaseAuth.getInstance();
+        btnCriar = (Button) findViewById(R.id.button2);
+        edtEmail = (EditText) findViewById(R.id.editText4);
+        edtSenha = (EditText) findViewById(R.id.editText3);
+        btnCriar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                criarUser(edtEmail.getText().toString(),edtSenha.getText().toString());
+            }
+        });
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+    private void criarUser(final String email, String senha){
+        mAuth.createUserWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(CadastroUser.this, "Erro ao Cadastrar",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            mAuth.sendPasswordResetEmail(email);
+                        }
+                    }
+                });
+    }
+}
