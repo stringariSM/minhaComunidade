ce827f91abec83c85bda213c0bb041a83e2e935f
Gabriel Stringari <gabriel@gabrielstringari.com>
2017-05-18 15:34:37 -0300

    Tela inicial

--- app/src/main/java/com/comunidadeapp/minhacomunidade/MainActivity.java
+++ app/src/main/java/com/comunidadeapp/minhacomunidade/MainActivity.java
@@ -1,10 +1,10 @@
 package com.comunidadeapp.minhacomunidade;
 
-import android.os.Build;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +12,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+        Button btnEmail = (Button) findViewById(R.id.btnSignup);
+        //Ao clicar no botão de cadastro
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this, CadastroUser.class);
+                startActivity(it);
+            }
+        });
+
+        Button btnFacebook = (Button) findViewById(R.id.btnFacebookSignup);
+        //Ao clicar no botão de cadastro om Facebook
+        btnFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //GO HORSE
+            }
+        });
+
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        //Ao clicar no botão de login
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(it);
+            }
+        });
     }
 }
