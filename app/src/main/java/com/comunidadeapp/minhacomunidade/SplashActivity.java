645ab190774a2dec4cc041fb6db2c54edf8d247c
Gabriel Stringari <gabriel@brainytalk.com>
2017-07-03 21:19:30 -0300

    Layout

--- app/src/main/java/com/comunidadeapp/minhacomunidade/SplashActivity.java
+++ app/src/main/java/com/comunidadeapp/minhacomunidade/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.comunidadeapp.minhacomunidade;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        overridePendingTransition(R.anim.left_go_in, R.anim.left_go_out);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent Main = new Intent(SplashActivity.this,MainActivity.class);
+        startActivity(Main);
+        finish();
+        overridePendingTransition(R.anim.left_back_in, R.anim.left_back_out);
     }
 }
