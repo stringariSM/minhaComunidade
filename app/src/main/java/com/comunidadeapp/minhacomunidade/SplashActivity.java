91bdd7c1973c08529c8c600434764a52d11aae3c
egasparotto <gasp.eduardo@gmail.com>
2017-07-03 22:41:43 -0300

    Upload de Fotos

--- app/src/main/java/com/comunidadeapp/minhacomunidade/SplashActivity.java
+++ app/src/main/java/com/comunidadeapp/minhacomunidade/SplashActivity.java
@@ -1,23 +1,137 @@
 package com.comunidadeapp.minhacomunidade;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class SplashActivity extends AppCompatActivity {
 
+    Button BaterFoto;
+    ProgressBar carregando;
+    private StorageReference mStorageRef;
+    File photoFile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        overridePendingTransition(R.anim.left_go_in, R.anim.left_go_out);
+        BaterFoto = (Button) findViewById(R.id.button2);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        carregando = (ProgressBar) findViewById(R.id.progressBar2);
+        carregando.setVisibility(View.GONE);
+        BaterFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.comunidadeapp.minhacomunidade.contentprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    public String EnviaFoto(String local){
+        InputStream stream = null;
+        try {
+            stream = new FileInputStream(new File(local));
+        }
+        catch(java.io.FileNotFoundException ex){
+            Toast toast = Toast.makeText(this,ex.getMessage(), Toast.LENGTH_SHORT);
+            toast.show();
+        };
+        Uri file = Uri.fromFile(new File(local));
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String LocalRemoto = "images/"+user.getUid()+"/"+file.getLastPathSegment();
+        StorageReference refFoto = mStorageRef.child(LocalRemoto);
+        UploadTask uploadTask = refFoto.putFile(file);
+        carregando.setVisibility(View.VISIBLE);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                Intent novoApontamento = new Intent(SplashActivity.this,Drawer.class);
+                novoApontamento.putExtra("URLFOTO", downloadUrl.toString());
+                startActivity(novoApontamento);
+            }
+        });
+        return local;
     }
 
     @Override
-    public void onBackPressed() {
-        Intent Main = new Intent(SplashActivity.this,MainActivity.class);
-        startActivity(Main);
-        finish();
-        overridePendingTransition(R.anim.left_back_in, R.anim.left_back_out);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            EnviaFoto(photoFile.getPath());
+        }
     }
 }
